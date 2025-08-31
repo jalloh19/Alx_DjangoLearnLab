8fc5a17d34fe12402cb8bf37e3d5e86ff0cb4f35
jalloh19 <ikdiallotechie@gmail.com>
2025-09-01 00:29:57 +0800

    Implement the Django Admin Interface

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    # Columns displayed in the list view
+    list_display = ('title', 'author', 'publication_year')
+
+    # Filters in the right sidebar
+    list_filter = ('author', 'publication_year')
+
+    # Search box on top
+    search_fields = ('title', 'author')

da5f846fb9a106d1c50c907dfdfb1968894d94cf
jalloh19 <ikdiallotechie@gmail.com>
2025-09-08 05:06:14 +0800

    Logi

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from .views import list_books, LibraryDetailView, home_view
+from .views import register_view, CustomLoginView, CustomLogoutView, home_view
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
     path('home/', home_view, name='home'),
     path('books/', list_books, name='list_books'),
     path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('register/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,6 +4,13 @@ from .models import Library
 from django.views.generic.detail import DetailView
 from django.shortcuts import render
 
+from django.shortcuts import render, redirect
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth import login
+from django.urls import reverse_lazy
+from .forms import CustomUserCreationForm
+from django.contrib.auth.forms import UserCreationForm
+
 def home_view(request):
     return render(request, 'relationship_app/home.html')
 
@@ -20,3 +27,22 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+
+def register_view(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
+
+class CustomLoginView(LoginView):
+    template_name = 'relationship_app/login.html'
+    redirect_authenticated_user = True
+
+class CustomLogoutView(LogoutView):
+    next_page = reverse_lazy('login')
